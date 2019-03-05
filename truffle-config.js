1b03903cd0fd22067cd01de2b22d2c257eb40801
merlox <merunasgrincalaitis@gmail.com>
2019-03-05 21:34:42 +0100

    Added several addresses for truffle testing with ropsten by changing the wallet provider in truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -66,7 +66,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`, 0, 50),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
@@ -84,6 +84,7 @@ module.exports = {
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
+      enableTimeouts: false
     // timeout: 100000
   },
 
